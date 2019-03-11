31c5580eefd27b18571c582af73842cacd6e3e96
T.J. Telan <tjtelan@level11.com>
2019-03-11 01:14:12 -0700

    Example Vault client code. KV version 1 only.

---
+++ vault/src/main.rs
@@ -0,0 +1,18 @@
+extern crate dotenv;
+extern crate hashicorp_vault as vault;
+
+use std::env;
+
+fn main() {
+    dotenv::dotenv().ok();
+
+    let host = "http://localhost:8200";
+    let token = "test";
+    let client = vault::Client::new(host, token).unwrap();
+
+    let _ = client.set_secret("foo", "bar");
+
+    let secret = client.get_secret("foo").unwrap();
+
+	println!("Secret is \"bar\": {}", secret);
+}
